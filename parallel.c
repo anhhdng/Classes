fba027968e2d87167de117115485fcfd45f8a8de
Anh Duong <90409711+anhhdng@users.noreply.github.com>
2023-10-20 22:28:37 -0400

    Update parallel.c

--- parallel.c
+++ parallel.c
@@ -4,95 +4,83 @@
 #include <string.h>
 
 #define MAX 10240
-#define NUM_THREADS  10
+#define NUM_THREADS 10
 
-int total = 0;
-int n1,n2;
-char *s1,*s2;
+int n1, n2;
+char *s1, *s2;
 FILE *fp;
-int countArray[NUM_THREADS]={0};
-pthread_mutex_t lock= PTHREAD_MUTEX_INITIALIZER;
+int totalNum = 0;
 
-//read input file and generate string s2/s2 and length n1/n2
+pthread_mutex_t lock;
+
+//read input file and generate string s1/s2 and length n1/n2
 int readf(FILE *fp)
 {
-	if((fp=fopen("strings.txt", "r"))==NULL){
-		printf("ERROR: can't open string.txt!\n");
-		return 0;
-	}
-	s1=(char *)malloc(sizeof(char)*MAX);
-	if(s1==NULL){
-		printf("ERROR: Out of memory!\n");
-		return -1;
-	}
-	s2=(char *)malloc(sizeof(char)*MAX);
-	if(s1==NULL){
-		printf("ERROR: Out of memory\n");
-		return -1;
-	}
-	/*read s1 s2 from the file*/
-	s1=fgets(s1, MAX, fp);
-	s2=fgets(s2, MAX, fp);
-	n1=strlen(s1);  /*length of s1*/
-	n2=strlen(s2)-1; /*length of s2*/
+    if ((fp = fopen("strings.txt", "r")) == NULL)
+    {
+        printf("ERROR: can't open string.txt!\n");
+        return 0;
+    }
+    s1 = (char *)malloc(sizeof(char) * MAX);
+    if (s1 == NULL)
+    {
+        printf("ERROR: Out of memory!\n");
+        return -1;
+    }
+    s2 = (char *)malloc(sizeof(char) * MAX);
+    if (s2 == NULL)
+    {
+        printf("ERROR: Out of memory\n");
+        return -1;
+    }
+    /*read s1 s2 from the file*/
+    s1 = fgets(s1, MAX, fp);
+    s2 = fgets(s2, MAX, fp);
+    n1 = strlen(s1); /*length of s1*/
+    n2 = strlen(s2) - 1; /*length of s2*/
 
-	if(s1==NULL || s2==NULL || n1<n2)  /*when error exit*/
-		return -1;
-	return 0;
+    if (s1 == NULL || s2 == NULL || n1 < n2) /*when error exit*/
+        return -1;
+    return 0;
 }
 
-int num_substring(int t)
+int num_substring(int start, int end)
 {
-//add your logic here
-//1, how to distribute different parts of string s1 into different threads
-/*
+    int count = 0;
 
-for 1, we have 10 threads in total, so divide s1 into 10 different equally threads.
-we take the length of s1, which is n1, and divide with the total num of threads, 10
---> int start = (n1/ NUM_THREADS) * tid to get the position where the search starts at
---> int end = (n1/NUM_THREADS) *(tid+1) at which string position the thread stop
-
-follow the same logic for single pthread outer for loop example but substitute i for start and i <= end so it can
-iterate through the divided threads
-
-*/
-int count;
-int i, j, k;
-int end = (n1/NUM_THREADS) *(t+1);
-int start = (n1/ NUM_THREADS) * t;
-
-for(i = start; i <= end;  i++){
-    for(j = i, k = 0, k  <n2; j++; k++){ //search for the string with size of n2 == search for "is" in 10 threads
-        if (*(s1+j) != *(s2+k)){
-            break;
-        } else{
-            count++; //count after each thread
+    for (int i = start; i <= end; i++)
+    {
+        int j, k;
+        for (j = i, k = 0; k < n2; j++, k++)
+        {
+            if (*(s1 + j) != *(s2 + k))
+            {
+                break;
+            }
         }
-        if(count==n2){
+        if (k == n2)
+        {
             count++;
         }
     }
-}
-return count;
 
-/*
-2, how to sum up the total number of substring from all threads
-to sum up the total number but also ensure that the count of each thread got printed out, we must
-place a lock to prevent the program to print out the total number after each thread
-after all threads are run, the function will return the total number of substring from all the threads
-*/
-
+    return count;
 }
 
-void *calSubStringThread(void *threadid){
-    long  tid =(long) threadid;
-    printf("This is thread %ld, ", tid);
-    int num = num_substring(tid);
-    printf("find num of is: %d\n", num);
+void *calSubStringThread(void *threadid)
+{
+    long tid = (long)threadid;
+    int start = (n1 / NUM_THREADS) * tid;
+    int end = (tid == NUM_THREADS - 1) ? n1 - 1 : start + (n1 / NUM_THREADS) - 1;
+
+    printf("This is thread %ld, searching from %d to %d\n", tid, start, end);
+
+    int num = num_substring(start, end);
+
     pthread_mutex_lock(&lock);
-    total += num;
+    totalNum += num;
     pthread_mutex_unlock(&lock);
-    
+
     pthread_exit(NULL);
 }
 
@@ -100,22 +88,26 @@ int main(int argc, char *argv[])
 {
     pthread_t threads[NUM_THREADS];
     int t, rc;
-    int totalNum = 0;
 
-	readf(fp);
+    readf(fp);
 
-	for(t=0; t<NUM_THREADS; t++){
-        rc = pthread_create(&threads[t], NULL, calSubStringThread, (void *) (size_t)t);
-        if (rc){
+    pthread_mutex_init(&lock, NULL);
+
+    for (t = 0; t < NUM_THREADS; t++)
+    {
+        rc = pthread_create(&threads[t], NULL, calSubStringThread, (void *)(size_t)t);
+        if (rc)
+        {
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
+
+        pthread_join(threads[t], NULL); // Wait for the thread to finish before starting the next one
     }
 
-    for(t=0; t<NUM_THREADS; t++){
-        pthread_join(threads[t], NULL);
-    }
+    printf("The number of substrings is: %d\n", totalNum);
 
- 	printf("The number of substrings is: %d\n", totalNum);
-	return 1;
+    pthread_mutex_destroy(&lock);
+
+    return 1;
 }
